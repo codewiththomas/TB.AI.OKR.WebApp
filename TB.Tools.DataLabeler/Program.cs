848b2fd13126aa7c3ed293b41c6db40377c71012
Thomas Bazan <thomas.bazan@tasag.de>
2023-10-03 18:19:25 +0200

    Added project for labeling data.

---
+++ TB.Tools.DataLabeler/Program.cs
@@ -0,0 +1,11 @@
+﻿// using ML to label the existing data
+
+using Microsoft.EntityFrameworkCore;
+using TB.AI.OKR.WebApp.Persistence.Contexts;
+
+using var applicationDbContext = new ApplicationDbContext();
+
+var rules = await applicationDbContext.OkrRules.ToListAsync();
+
+//label okr sets
+
