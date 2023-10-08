747ffc5a63b5efbcf724de121824dc45177152b9
Thomas Bazan <thomas.bazan@gmail.com>
2023-10-08 10:09:27 +0200

    Added API controllers for readability service

---
+++ TB.AI.OKR.WebApp/Controllers/ReadabilityController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TB.Tools.Readability;
+
+namespace TB.AI.OKR.WebApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReadabilityController : ControllerBase
+{
+    private IReadabilityService ReadabilityService { get; }
+
+
+    public ReadabilityController(IReadabilityService readabilityService)
+    {
+        ReadabilityService = readabilityService;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="algorithm">if left blank, ARI will be used</param>
+    /// <returns></returns>
+    [HttpGet("rate")]
+    public IActionResult GetReadadabilityRating(string text, string? algorithm = null)
+    {
+        if (algorithm is null)
+        {
+            algorithm = ReadabilityAlgorithms.AutomatedReadabilityIndex.ToString();
+        }
+
+        try
+        {
+            return Ok(ReadabilityService.CalculateReadability(text, algorithm));
+        }
+        catch (ArgumentException ex)
+        { 
+            return BadRequest(ex.Message);
+        }
+       
+        
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("algorithms")]
+    public IActionResult GetAlgorithms()
+    {
+        return Ok(ReadabilityService.GetAvailableAlgorithms());
+    }
+}
--- TB.AI.OKR.WebApp/Program.cs
+++ TB.AI.OKR.WebApp/Program.cs
@@ -3,6 +3,8 @@ using TB.GPT4All.ApiClient;
 using TB.AI.OKR.WebApp.Persistence.Contexts;
 using Microsoft.Fast.Components.FluentUI;
 using TB.AI.OKR.WebApp.Persistence.Repositories;
+using Microsoft.OpenApi.Models;
+using TB.Tools.Readability;
 
 namespace TB.AI.OKR.WebApp;
 
@@ -13,13 +15,22 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         #region Add services to the container.
-        
+
+        #region API support 
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen(x =>
+        {
+            x.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+        });
+        #endregion
+
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
         
         /* Include the self-written ApiClient */
         builder.Services.AddOpenAiApiClient(builder.Configuration);
         builder.Services.AddGPT4AllApiClient(builder.Configuration);
+        builder.Services.AddReadabilityService();
 
         /* Add persistence layer */
         builder.Services.AddDbContext<ApplicationDbContext>();
@@ -49,11 +60,22 @@ public class Program
             app.UseHsts();
         }
 
+
+
         app.UseHttpsRedirection();
 
         app.UseStaticFiles();
 
         app.UseRouting();
+
+        #region API support
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        });
+        app.MapControllers(); //needed for API-Support
+        #endregion
 
         app.MapBlazorHub();
         app.MapFallbackToPage("/_Host");
