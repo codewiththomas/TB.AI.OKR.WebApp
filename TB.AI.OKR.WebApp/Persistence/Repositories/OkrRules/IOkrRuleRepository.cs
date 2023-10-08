3f05ee3bb3ab7ca4a5f331174ca43ce453b89c27
Thomas Bazan <thomas.bazan@gmail.com>
2023-10-08 14:09:49 +0200

    Added add/edit for OKR rules

--- TB.AI.OKR.WebApp/Persistence/Repositories/OkrRules/IOkrRuleRepository.cs
+++ TB.AI.OKR.WebApp/Persistence/Repositories/OkrRules/IOkrRuleRepository.cs
@@ -4,6 +4,10 @@ namespace TB.AI.OKR.WebApp.Persistence.Repositories;
 
 public interface IOkrRuleRepository
 {
+    Task<OkrRule> AddAsync(AddOkrRuleDto addOkrRuleDto);
+
+    Task<OkrRule?> GetAsync(int id);
     Task<IEnumerable<OkrRule>> GetAllAsync();
+    Task<OkrRule> UpdateAsync(UpdateOkrRuleDto updateOkrRuleDto);
     Task<bool> UpdateActiveStatus(int okrRuleId, bool activeStatus);
 }
---
+++ TB.AI.OKR.WebApp/Persistence/Repositories/OkrRules/Update/UpdateOkrRuleDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TB.AI.OKR.WebApp.Persistence.Repositories;
+
+public class UpdateOkrRuleDto
+{
+    public int Id { get; set; }
+
+    [MaxLength(150)]
+    [Required]
+    public string OkrRuleTitle { get; set; } = string.Empty;
+
+    [Required]
+    public string OkrRuleText { get; set; } = string.Empty;
+
+    public OkrRuleScopes Scope { get; set; }
+
+    public OkrRuleSeverities Severity { get; set; }
+
+    public IList<int> ReferenceSourceIds { get; set; }
+        = new List<int>();
+}
