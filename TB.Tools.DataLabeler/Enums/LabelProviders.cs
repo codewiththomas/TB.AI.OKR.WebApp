8b320e2a29c647418627d5924368b03198ecf02b
Thomas Bazan <thomas.bazan@tasag.de>
2023-10-27 17:24:04 +0200

    Added enum for providers

---
+++ TB.Tools.DataLabeler/Enums/LabelProviders.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace TB.Tools.DataLabeler;
+
+public enum LabelProviders
+{
+    [Description("Annotator")]
+    Base = 0,
+
+    [Description("gpt-3.5-turbo")]
+    OpenAI_gpt35turbo = 135,
+    OpenAI_gpt4 = 140,
+
+    [Description("gpt4all-falcon-q4_0.gguf")]
+    GPT4All_falcon = 200,
+
+    [Description("nous-hermes-llama2-13b.Q4_0.gguf")]
+    GPT4All_hermes_llama2 = 201,
+
+    [Description("Machine Learning Model from ML.NET")]
+    ML = 300
+}
