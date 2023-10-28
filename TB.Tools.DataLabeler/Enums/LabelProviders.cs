3f9f8bd26aa1ec7c3e0407a83f49a73bbb7ce755
Thomas Bazan <thomas.bazan@gmail.com>
2023-10-28 10:19:48 +0200

    Changed names of providers

--- TB.Tools.DataLabeler/Enums/LabelProviders.cs
+++ TB.Tools.DataLabeler/Enums/LabelProviders.cs
@@ -5,18 +5,18 @@ namespace TB.Tools.DataLabeler;
 public enum LabelProviders
 {
     [Description("Annotator")]
-    Base = 0,
+    Annotator = 0,
 
     [Description("gpt-3.5-turbo")]
-    OpenAI_gpt35turbo = 135,
-    OpenAI_gpt4 = 140,
+    OpenAI_GPT_35_Turbo = 135,
+    OpenAI_GPT_4 = 140,
 
     [Description("gpt4all-falcon-q4_0.gguf")]
-    GPT4All_falcon = 200,
+    GPT4All_Falcon = 200,
 
     [Description("nous-hermes-llama2-13b.Q4_0.gguf")]
-    GPT4All_hermes_llama2 = 201,
+    GPT4All_Hermes_LLaMA2 = 201,
 
-    [Description("Machine Learning Model from ML.NET")]
+    [Description("ml.net")]
     ML = 300
 }
