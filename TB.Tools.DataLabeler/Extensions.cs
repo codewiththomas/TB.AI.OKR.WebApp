d65dfb72743b6fbc722125003ffc28e6318eed8d
Thomas Bazan <thomas.bazan@gmail.com>
2023-10-28 10:19:27 +0200

    Extension method for getting model name by provider.

---
+++ TB.Tools.DataLabeler/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace TB.Tools.DataLabeler;
+
+public static class Extensions
+{
+    public static string GetOpenAiModelName(this LabelProviders labelProvider)
+    {
+        var result = labelProvider switch
+        {
+            LabelProviders.Annotator => "gpt-3.5-turbo",
+            LabelProviders.OpenAI_GPT_35_Turbo => "gpt-3.5-turbo",
+            LabelProviders.OpenAI_GPT_4 => "gpt-4",
+            _ => string.Empty
+        };
+        return result;
+    }
+}
